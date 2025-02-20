6846064a409b1da165115e6299ae437217979d38
Nastya <lalalamistikc31@gmail.com>
2025-02-20 16:04:12 +0500

    Разработана страница СППП

--- UP/Pages/PagesInTable/SPPP_Meetings.xaml.cs
+++ UP/Pages/PagesInTable/SPPP_Meetings.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using ClassConnection;
 
 namespace UP.Pages.PagesInTable
 {
@@ -20,9 +21,73 @@ namespace UP.Pages.PagesInTable
     /// </summary>
     public partial class SPPP_Meetings : Page
     {
-        public SPPP_Meetings()
+        ClassModules.SPPP_Meetings sppp;
+        public SPPP_Meetings(ClassModules.SPPP_Meetings _sppp)
         {
             InitializeComponent();
+            sppp = _sppp;
+            if (_sppp.OsnVizov != null)
+            {
+                RentStart.Text = _sppp.Date.ToString();
+                osnVizov.Text = _sppp.OsnVizov;
+                sotrud.Text = _sppp.Sotrudniki;
+                predstav.Text = _sppp.Predstaviteli;
+                prichVizov.Text = _sppp.ReasonForCall;
+                resh.Text = _sppp.Reshenie;
+                primech.Text = _sppp.Note;
+            }
+        }
+
+        private void Click_SPPP_Meetings_Redact(object sender, RoutedEventArgs e)
+        {
+            int id = Login.Login.connection.SetLastId(ClassConnection.Connection.Tables.SPPP_Meetings);
+            if (sppp.OsnVizov == null)
+            {
+                string query = $"Insert Into Departments ([DepartmentID], [DepartmentName]) Values ({id.ToString()}, '{DepName.Text}')";
+                var query_apply = Login.Login.connection.ExecuteQuery(query);
+                if (query_apply != null)
+                {
+                    Login.Login.connection.LoadData(ClassConnection.Connection.Tables.SPPP_Meetings);
+                    MainWindow.main.Animation_move(MainWindow.main.frame_main, MainWindow.main.scroll_main, null, null, Main.page_main.SPPP_Meetings);
+                }
+                else MessageBox.Show("Запрос на добавление цеха не был обработан!", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+            else
+            {
+                string query = $"Update сeh Departments [DepartmentName] = N'{DepName.Text}' Where [DepartmentID] = {obshaga.DormitoryID}";
+                var query_apply = Login.Login.connection.ExecuteQuery(query);
+                if (query_apply != null)
+                {
+                    Login.Login.connection.LoadData(ClassConnection.Connection.Tables.SPPP_Meetings);
+                    MainWindow.main.Animation_move(MainWindow.main.frame_main, MainWindow.main.scroll_main, null, null, Main.page_main.SPPP_Meetings);
+                }
+                else MessageBox.Show("Запрос на изменение цеха не был обработан!", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+        }
+
+        private void Click_Cancel_SPPP_Meetings_Redact(object sender, RoutedEventArgs e)
+        {
+            MainWindow.main.Animation_move(MainWindow.main.frame_main, MainWindow.main.scroll_main);
+        }
+
+        private void Click_Remove_SPPP_Meetings_Redact(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Login.Login.connection.LoadData(ClassConnection.Connection.Tables.SPPP_Meetings);
+                string query = "Delete Departments Where [DepartmentID] = " + obshaga.DormitoryID.ToString() + "";
+                var query_apply = Login.Login.connection.ExecuteQuery(query);
+                if (query_apply != null)
+                {
+                    Login.Login.connection.LoadData(ClassConnection.Connection.Tables.SPPP_Meetings);
+                    Main.main.Animation_move(MainWindow.main.frame_main, MainWindow.main.scroll_main, null, null, Main.page_main.SPPP_Meetings);
+                }
+                else MessageBox.Show("Запрос на удаление цеха не был обработан!", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
     }
 }
