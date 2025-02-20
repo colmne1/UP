c9e3bc50f247eb5b091fde073dd632db85b2e79e
Nastya <lalalamistikc31@gmail.com>
2025-02-20 16:03:22 +0500

    Разработана страница социальной стипендии

--- UP/Pages/PagesInTable/SocialScholarships.xaml.cs
+++ UP/Pages/PagesInTable/SocialScholarships.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using ClassConnection;
 
 namespace UP.Pages.PagesInTable
 {
@@ -20,9 +21,69 @@ namespace UP.Pages.PagesInTable
     /// </summary>
     public partial class SocialScholarships : Page
     {
-        public SocialScholarships()
+        ClassModules.SocialScholarships social;
+        public SocialScholarships(ClassModules.SocialScholarships _social)
         {
             InitializeComponent();
+            social = _social;
+            if (_social.DocumentReason != null)
+            {
+                docOsn.Text = _social.DocumentReason;
+                nachVipl.Text = _social.StartDate.ToString();
+                konVipl.Text = _social.EndDate.ToString();
+            }
+        }
+
+        private void Click_SocialScholarships_Redact(object sender, RoutedEventArgs e)
+        {
+            int id = Login.Login.connection.SetLastId(ClassConnection.Connection.Tables.SocialScholarships);
+            if (social.DocumentReason == null)
+            {
+                string query = $"Insert Into Departments ([DepartmentID], [DepartmentName]) Values ({id.ToString()}, '{DepName.Text}')";
+                var query_apply = Login.Login.connection.ExecuteQuery(query);
+                if (query_apply != null)
+                {
+                    Login.Login.connection.LoadData(ClassConnection.Connection.Tables.SocialScholarships);
+                    MainWindow.main.Animation_move(MainWindow.main.frame_main, MainWindow.main.scroll_main, null, null, Main.page_main.SocialScholarships);
+                }
+                else MessageBox.Show("Запрос на добавление цеха не был обработан!", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+            else
+            {
+                string query = $"Update сeh Departments [DepartmentName] = N'{DepName.Text}' Where [DepartmentID] = {obshaga.DormitoryID}";
+                var query_apply = Login.Login.connection.ExecuteQuery(query);
+                if (query_apply != null)
+                {
+                    Login.Login.connection.LoadData(ClassConnection.Connection.Tables.SocialScholarships);
+                    MainWindow.main.Animation_move(MainWindow.main.frame_main, MainWindow.main.scroll_main, null, null, Main.page_main.SocialScholarships);
+                }
+                else MessageBox.Show("Запрос на изменение цеха не был обработан!", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+        }
+
+        private void Click_Cancel_SocialScholarships_Redact(object sender, RoutedEventArgs e)
+        {
+            MainWindow.main.Animation_move(MainWindow.main.frame_main, MainWindow.main.scroll_main);
+        }
+
+        private void Click_Remove_SocialScholarships_Redact(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Login.Login.connection.LoadData(ClassConnection.Connection.Tables.SocialScholarships);
+                string query = "Delete Departments Where [DepartmentID] = " + obshaga.DormitoryID.ToString() + "";
+                var query_apply = Login.Login.connection.ExecuteQuery(query);
+                if (query_apply != null)
+                {
+                    Login.Login.connection.LoadData(ClassConnection.Connection.Tables.SocialScholarships);
+                    Main.main.Animation_move(MainWindow.main.frame_main, MainWindow.main.scroll_main, null, null, Main.page_main.SocialScholarships);
+                }
+                else MessageBox.Show("Запрос на удаление цеха не был обработан!", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
     }
 }
