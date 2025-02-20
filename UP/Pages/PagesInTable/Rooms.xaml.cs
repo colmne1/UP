8711811f4007c64accd252a1ee6e4ddc8ede74c6
Nastya <lalalamistikc31@gmail.com>
2025-02-20 16:08:04 +0500

    Разработана страница комнат

--- UP/Pages/PagesInTable/Rooms.xaml.cs
+++ UP/Pages/PagesInTable/Rooms.xaml.cs
@@ -20,9 +20,68 @@ namespace UP.Pages.PagesInTable
     /// </summary>
     public partial class Rooms : Page
     {
-        public Rooms()
+        ClassModules.Rooms rooms;
+        public Rooms(ClassModules.Rooms _rooms)
         {
             InitializeComponent();
+            rooms = _rooms;
+            if (_rooms.RoomName != null)
+            {
+                nameroom.Text = _rooms.RoomName;
+                vmestim.Text = _rooms.Vmestim;
+            }
+        }
+
+        private void Click_Rooms_Redact(object sender, RoutedEventArgs e)
+        {
+            int id = Login.Login.connection.SetLastId(ClassConnection.Connection.Tables.Rooms);
+            if (rooms.RoomName == null)
+            {
+                string query = $"Insert Into Departments ([DepartmentID], [DepartmentName]) Values ({id.ToString()}, '{DepName.Text}')";
+                var query_apply = Login.Login.connection.ExecuteQuery(query);
+                if (query_apply != null)
+                {
+                    Login.Login.connection.LoadData(ClassConnection.Connection.Tables.Rooms);
+                    MainWindow.main.Animation_move(MainWindow.main.frame_main, MainWindow.main.scroll_main, null, null, Main.page_main.Rooms);
+                }
+                else MessageBox.Show("Запрос на добавление цеха не был обработан!", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+            else
+            {
+                string query = $"Update сeh Departments [DepartmentName] = N'{DepName.Text}' Where [DepartmentID] = {departments.DepartmentID}";
+                var query_apply = Login.Login.connection.ExecuteQuery(query);
+                if (query_apply != null)
+                {
+                    Login.Login.connection.LoadData(ClassConnection.Connection.Tables.Rooms);
+                    MainWindow.main.Animation_move(MainWindow.main.frame_main, MainWindow.main.scroll_main, null, null, Main.page_main.Rooms);
+                }
+                else MessageBox.Show("Запрос на изменение цеха не был обработан!", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+        }
+
+        private void Click_Cancel_Rooms_Redact(object sender, RoutedEventArgs e)
+        {
+            MainWindow.main.Animation_move(MainWindow.main.frame_main, MainWindow.main.scroll_main);
+        }
+
+        private void Click_Remove_Rooms_Redact(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Login.Login.connection.LoadData(ClassConnection.Connection.Tables.Rooms);
+                string query = "Delete Departments Where [DepartmentID] = " + departments.DepartmentID.ToString() + "";
+                var query_apply = Login.Login.connection.ExecuteQuery(query);
+                if (query_apply != null)
+                {
+                    Login.Login.connection.LoadData(ClassConnection.Connection.Tables.Rooms);
+                    Main.main.Animation_move(MainWindow.main.frame_main, MainWindow.main.scroll_main, null, null, Main.page_main.Rooms);
+                }
+                else MessageBox.Show("Запрос на удаление цеха не был обработан!", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
     }
 }
