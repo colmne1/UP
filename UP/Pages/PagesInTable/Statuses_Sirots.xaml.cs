c14aa5ca717949b2991b05d81f1bbee04f53a3f5
Nastya <lalalamistikc31@gmail.com>
2025-02-20 16:04:50 +0500

    Разработана страница сирот

--- UP/Pages/PagesInTable/Statuses_Sirots.xaml.cs
+++ UP/Pages/PagesInTable/Statuses_Sirots.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using ClassConnection;
 
 namespace UP.Pages.PagesInTable
 {
@@ -20,9 +21,69 @@ namespace UP.Pages.PagesInTable
     /// </summary>
     public partial class Statuses_Sirots : Page
     {
-        public Statuses_Sirots()
+        ClassModules.Statuses_Sirots sirots;
+        public Statuses_Sirots(ClassModules.Statuses_Sirots _sirots)
         {
             InitializeComponent();
+            sirots = _sirots;
+            if (_sirots.OrderNumber != null)
+            {
+                prikaz.Text = _sirots.OrderNumber;
+                nachStat.Text = _sirots.StartDate.ToString();
+                konStat.Text = _sirots.EndDate.ToString();
+                primech.Text = _sirots.Note;
+            }
+        }
+        private void Click_Statuses_Sirots_Redact(object sender, RoutedEventArgs e)
+        {
+            int id = Login.Login.connection.SetLastId(ClassConnection.Connection.Tables.Statuses_Sirots);
+            if (sirots.OrderNumber == null)
+            {
+                string query = $"Insert Into Departments ([DepartmentID], [DepartmentName]) Values ({id.ToString()}, '{DepName.Text}')";
+                var query_apply = Login.Login.connection.ExecuteQuery(query);
+                if (query_apply != null)
+                {
+                    Login.Login.connection.LoadData(ClassConnection.Connection.Tables.Statuses_Sirots);
+                    MainWindow.main.Animation_move(MainWindow.main.frame_main, MainWindow.main.scroll_main, null, null, Main.page_main.Statuses_Sirots);
+                }
+                else MessageBox.Show("Запрос на добавление цеха не был обработан!", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+            else
+            {
+                string query = $"Update сeh Departments [DepartmentName] = N'{DepName.Text}' Where [DepartmentID] = {obshaga.DormitoryID}";
+                var query_apply = Login.Login.connection.ExecuteQuery(query);
+                if (query_apply != null)
+                {
+                    Login.Login.connection.LoadData(ClassConnection.Connection.Tables.Statuses_Sirots);
+                    MainWindow.main.Animation_move(MainWindow.main.frame_main, MainWindow.main.scroll_main, null, null, Main.page_main.Statuses_Sirots);
+                }
+                else MessageBox.Show("Запрос на изменение цеха не был обработан!", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+        }
+
+        private void Click_Cancel_Statuses_Sirots_Redact(object sender, RoutedEventArgs e)
+        {
+            MainWindow.main.Animation_move(MainWindow.main.frame_main, MainWindow.main.scroll_main);
+        }
+
+        private void Click_Remove_Statuses_Sirots_Redact(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Login.Login.connection.LoadData(ClassConnection.Connection.Tables.Statuses_Sirots);
+                string query = "Delete Departments Where [DepartmentID] = " + obshaga.DormitoryID.ToString() + "";
+                var query_apply = Login.Login.connection.ExecuteQuery(query);
+                if (query_apply != null)
+                {
+                    Login.Login.connection.LoadData(ClassConnection.Connection.Tables.Statuses_Sirots);
+                    Main.main.Animation_move(MainWindow.main.frame_main, MainWindow.main.scroll_main, null, null, Main.page_main.Statuses_Sirots);
+                }
+                else MessageBox.Show("Запрос на удаление цеха не был обработан!", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
     }
 }
