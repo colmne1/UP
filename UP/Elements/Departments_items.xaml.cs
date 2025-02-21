76ad4c0fac5b461e1822cc9cca8bbee45112581b
Nastya <lalalamistikc31@gmail.com>
2025-02-21 14:24:39 +0500

    Разработан пользовательский элемент управления отделений

--- UP/Elements/Departments_items.xaml.cs
+++ UP/Elements/Departments_items.xaml.cs
@@ -20,9 +20,41 @@ namespace UP.Elements
     /// </summary>
     public partial class Departments_items : UserControl
     {
-        public Departments_items()
+        ClassModules.Departments Departments;
+        public Departments_items(ClassModules.Departments _Departments)
         {
             InitializeComponent();
+            if (Pages.Login.Login.UserInfo[1] != "admin") Buttons.Visibility = Visibility.Hidden;
+            Departments = _Departments;
+            if (_Departments.DepartmentName != null)
+            {
+                otdel_name.Content = "Цех №" + _Departments.DepartmentID.ToString();
+                name.Content = "Оборудование: " + _Departments.DepartmentName;
+            }
+        }
+        private void Click_redact(object sender, RoutedEventArgs e) => MainWindow.main.Animation_move(MainWindow.main.scroll_main, MainWindow.main.frame_main, MainWindow.main.frame_main, new Pages.PagesInTable.Departments(Departments));
+
+        private void Click_remove(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (MessageBox.Show("Вы уверены, что хотите удалить информацию о отделениях?", "Удаление информации", MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes)
+                {
+                    Pages.Login.Login.connection.LoadData(ClassConnection.Connection.Tables.Departments);
+                    string query = $"Delete Departments сeh Where DepartmentID = " + Departments.DepartmentID.ToString() + "";
+                    var query_apply = Pages.Login.Login.connection.ExecuteQuery(query);
+                    if (query_apply != null)
+                    {
+                        Pages.Login.Login.connection.LoadData(ClassConnection.Connection.Tables.Departments);
+                        MainWindow.main.Animation_move(MainWindow.main.frame_main, MainWindow.main.scroll_main, null, null, Pages.Main.page_main.Departments);
+                    }
+                    else MessageBox.Show("Запрос на удаление отделений не был обработан!", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Warning);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+            }
         }
     }
 }
