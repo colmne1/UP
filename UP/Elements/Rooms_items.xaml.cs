f7446c627e0082429defd1008c97dc24d106179c
Nastya <lalalamistikc31@gmail.com>
2025-02-21 14:25:02 +0500

    Разработан пользовательский элемент управления комнат

--- UP/Elements/Rooms_items.xaml.cs
+++ UP/Elements/Rooms_items.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using ClassConnection;
 
 namespace UP.Elements
 {
@@ -20,9 +21,44 @@ namespace UP.Elements
     /// </summary>
     public partial class Rooms_items : UserControl
     {
-        public Rooms_items()
+        ClassModules.Rooms rooms;
+        public Rooms_items(ClassModules.Rooms _rooms)
         {
             InitializeComponent();
+            if (Pages.Login.Login.UserInfo[1] != "admin") Buttons.Visibility = Visibility.Hidden;
+            rooms = _rooms;
+            if (_rooms.RoomName != null)
+            {
+                room_name.Content += _rooms.RoomID.ToString();
+                name.Content += _rooms.RoomName;
+                vmestim.Content += _rooms.Vmestim.ToString();
+
+            }
+        }
+
+        private void Click_redact(object sender, RoutedEventArgs e) => MainWindow.main.Animation_move(MainWindow.main.scroll_main, MainWindow.main.frame_main, MainWindow.main.frame_main, new Pages.PagesInTable.Rooms(rooms));
+
+        private void Click_remove(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (MessageBox.Show("Вы уверены, что хотите удалить информацию о комнате?", "Удаление информации", MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes)
+                {
+                    Pages.Login.Login.connection.LoadData(ClassConnection.Connection.Tables.Rooms);
+                    string query = $"Delete From Rooms Where RoomID = " + rooms.RoomID.ToString() + "";
+                    var query_apply = Pages.Login.Login.connection.ExecuteQuery(query);
+                    if (query_apply != null)
+                    {
+                        Pages.Login.Login.connection.LoadData(ClassConnection.Connection.Tables.Rooms);
+                        MainWindow.main.Animation_move(MainWindow.main.frame_main, MainWindow.main.scroll_main, null, null, Pages.Main.page_main.Rooms);
+                    }
+                    else MessageBox.Show("Запрос на удаление комнаты не был обработан!", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Warning);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+            }
         }
     }
 }
